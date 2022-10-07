d52731d9edb740369fe62aaa25d27c6f80feb4af
August Reid <c.august.reid@gmail.com>
2022-10-07 16:02:27 -0600

    Add Home component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@
 // import './App.css';
 import Header from "./Components/Header";
 import { Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
 
 function App() {
   return (
     <main>
       <Header/>
       <Routes>
-        
+        <Route path={"/"} element={<Home />} />
       </Routes>
     </main>
   );
